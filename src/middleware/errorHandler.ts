e10e2f09dd21b3ec29c132480e5824e490713dcc
Mariano Aguero <mariano.aguero@gmail.com>
2023-04-21 15:02:39 -0300

    feat: add github workflow

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -4,6 +4,7 @@ import config from '../config'
 /**
  * 500 response & log when errors are raised.
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error('errorHandler', err)
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
