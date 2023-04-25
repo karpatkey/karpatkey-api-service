90b87a9945c1ed9534fdcc202ca2330939b1e583
Mariano Aguero <mariano.aguero@gmail.com>
2023-04-25 20:13:00 -0300

    feat: check test

--- src/tests/aum.spec.ts
+++ src/tests/aum.spec.ts
@@ -1,8 +1,10 @@
 import supertest from 'supertest'
 import app from '../app'
+import config from '../config'
 
 describe('aum endpoint get', () => {
   test('get aum returns 200 and data', async () => {
+    console.log('config email' + config.googleCredentials.client_email)
     const result = await supertest(app).get('/aum')
     expect(result.statusCode).toEqual(200)
     expect(result.body.value).toBeTruthy()
